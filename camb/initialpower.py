5b647287ae5565e24509a11b3a8a5fff06005f56
ThijsRozeboom <106308340+ThijsRozeboom@users.noreply.github.com>
2022-05-26 12:41:17 +0200

    Update initialpower.py

--- camb/initialpower.py
+++ camb/initialpower.py
@@ -113,8 +113,7 @@ class InitialPowerLaw(InitialPower):
         ("pivot_scalar", c_double),
         ("pivot_tensor", c_double),
         ("As", c_double),
-        ("At", c_double),
-        ("TNP", c_double)
+        ("At", c_double)
     ]
 
     _fortran_class_name_ = 'TInitialPowerLaw'
@@ -122,7 +121,7 @@ class InitialPowerLaw(InitialPower):
     def __init__(self, **kwargs):
         self.set_params(**kwargs)
 
-    def set_params(self, As=2e-9, ns=0.96, nrun=0, nrunrun=0.0, r=0.0, nt=None, ntrun=0.0, TNP= 0.5,
+    def set_params(self, As=2e-9, ns=0.96, nrun=0, nrunrun=0.0, r=0.0, nt=None, ntrun=0.0, 
                    pivot_scalar=0.05, pivot_tensor=0.05, parameterization="tensor_param_rpivot"):
         r"""
         Set parameters using standard power law parameterization. If nt=None, uses inflation consistency relation.
