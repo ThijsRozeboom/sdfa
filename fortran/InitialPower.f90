7a5ccb1bed0eb04631ab793cd5c5fa342a1e62b3
ThijsRozeboom <106308340+ThijsRozeboom@users.noreply.github.com>
2022-05-27 14:38:15 +0200

    Update InitialPower.f90

--- fortran/InitialPower.f90
+++ fortran/InitialPower.f90
@@ -213,9 +213,9 @@
     this%ns = Ini%Read_Double(CompatKey(Ini,'scalar_spectral_index'))
     
     call Ini%Read(CompatKey(Ini,'scalar_nrun'), this%nrun)
-    call Ini%Read(CompatKey(Ini,'ampt'))
-    call Ini%Read(CompatKey(Ini,'freqt'))
-    call Ini%Read(CompatKey(Ini,'phaset'))
+    call Ini%Read(CompatKey(Ini,'ampt'), this%ampt)
+    call Ini%Read(CompatKey(Ini,'freqt'), this%freqt)
+    call Ini%Read(CompatKey(Ini,'phaset'), this%phaset)
     call Ini%Read(CompatKey(Ini,'scalar_nrunrun'), this%nrunrun)
 
     if (WantTensors) then
