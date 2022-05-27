4109a08bfaea43ba1e883fd57d47f5e09257cf16
ThijsRozeboom <106308340+ThijsRozeboom@users.noreply.github.com>
2022-05-27 11:00:25 +0200

    Update InitialPower.f90

--- fortran/InitialPower.f90
+++ fortran/InitialPower.f90
@@ -140,9 +140,8 @@
 
 
     lnrat = log(k/this%pivot_scalar)
-    TInitialPowerLaw_ScalarPower =this%As * exp(lnrat * (this%ns - 1 + &
-        &             lnrat * (this%nrun / 2 + this%nrunrun / 6 * lnrat)))
-        !TInitialPowerLaw_ScalarPower = TInitialPowerLaw_ScalarPower(1+ this%new_par *cos(lnrat*30))
+    TInitialPowerLaw_ScalarPower =(this%As * exp(lnrat * (this%ns - 1 + &
+        &             lnrat * (this%nrun / 2 + this%nrunrun / 6 * lnrat)))) * (1+ this%new_par * cos(lnrat*30))
 
     end function TInitialPowerLaw_ScalarPower
 
